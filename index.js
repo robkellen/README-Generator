516669bcccfc320e69bcfa670b85f022bf0a725d
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-26 16:06:53 -0700

    Generated prompts to get user data

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+// prompt user for questions
+function userPrompt(){
+  return inquirer.prompt([
+    {
+      message: "What is your GitHub user name?",
+      name: "username"
+    },
+    {
+      message: "What is the title of your project?",
+      name: "title"
+    },
+    {
+      message: "What is the description of your project?",
+      name: "description"
+    },
+    {
+      message: "What are the installation instructions for your project?",
+      name: "install"
+    },
+    {
+      message: "Please provide instructions and examples for use.",
+      name: "usage"
+    },
+    {
+      message: "Please provide HTML link to screenshot of deployed application.",
+      name: "screenshot"
+    },
+    {
+      message: "Please provide guidelines for how other developers can contribute to your project.",
+      name: "contributing"
+    },
+    {
+      message: "Please provide examples of tests to run in your application, and instructions on how to run them.",
+      name: "tests"
+    }
+  
+  ]).then(function(data){
+    console.log(data);
+  })
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -13,4 +55,5 @@ function init() {
 }
 
 // function call to initialize program
+userPrompt();
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  
+  ## ${data.tableofcontents}
+
+  ## ${data.license}
+
 
 `;
 }
