999db9918c4530da8256da3d2cf494e9cbd41d8f
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-28 09:13:13 -0700

    realized I had updated the generate markdown file in homework assignment folder and not my project folder...smh

--- index.js
+++ index.js
@@ -1,60 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const userPrompts = require("./utils/userPrompts");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
-// // prompt user for questions
-// function userPrompt(){
-//   return inquirer.prompt([
-//     {
-//       message: "What is your GitHub user name?",
-//       name: "username"
-//     },
-//     {
-//       message: "What is the title of your project?",
-//       name: "title"
-//     },
-//     {
-//       message: "What is the description of your project?",
-//       name: "description"
-//     },
-//     {
-//       message: "What are the installation instructions for your project?",
-//       name: "install"
-//     },
-//     {
-//       message: "Please provide instructions and examples for use.",
-//       name: "usage"
-//     },
-//     {
-//       message: "Please provide HTML link to screenshot of deployed application.",
-//       name: "screenshot"
-//     },
-//     {
-//       message: "Please provide guidelines for how other developers can contribute to your project.",
-//       name: "contributing"
-//     },
-//     {
-//       message: "Please provide examples of tests to run in your application, and instructions on how to run them.",
-//       name: "tests"
-//     }
-  
-//   ]).then(function(data){
-//     console.log(data);
-//   })
-// }
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName + ".md", JSON.stringify(data, null, 2), function(err){
+    console.log("Done");
+  })
 }
 
 // function to initialize program
-function init() {
+async function init() {
+  let data = await userPrompts();
+  let md = generateMarkdown(data);
+  writeToFile("testFile", md);
 
 }
 
 // function call to initialize program
-userPrompts();
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,44 @@
-// function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadgeUrl = "https://img.shields.io/github/license/" + data.username + "/" + data.repo;
+  console.log(licenseBadgeUrl);
   return `
   # ${data.title}
+  ${licenseBadgeUrl}
   
-  ## ${data.tableofcontents}
+  ## Table of Contents
+    * [Description] (#description)
+    * [Installation] (#installation)
+    * [Usage] (#usage)
+    * [License] (#license)
+    * [Contributing] (#contributing)
+    * [Tests] (#tests)
+    * [Questions] (#questions)
+  ##  
+  
+  ## Description
+  ${data.description}
 
-  ## ${data.license}
 
+  ## Installation 
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  "Please email me at " + ${data.questions} + "with any questions about this project."
 
 `;
 }
 
 module.exports = generateMarkdown;
+
