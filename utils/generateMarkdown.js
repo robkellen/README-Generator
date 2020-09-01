a33e80c3f4a6f12a922a48f82fe54b08d29cc827
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-01 09:18:32 -0700

    updated formatting for TOC

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -55,13 +55,14 @@ function generateMarkdown({
   ${licenseBadge}
   
   ## Table of Contents
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
+
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
     
   
   ## Description
